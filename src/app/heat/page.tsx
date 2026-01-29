3a4ca8fc28a37692bd67ce4e49c98f2259d1ca8e
indigoandlavender <happy@indigoandlavender.love>
2026-01-29 18:11:38 +0100

    Add files via upload

--- src/app/heat/page.tsx
+++ src/app/heat/page.tsx
@@ -288,7 +288,7 @@ export default function HeatMapPage() {
             {...viewState}
             onMove={(evt) => setViewState(evt.viewState)}
             mapStyle="mapbox://styles/mapbox/light-v11"
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN || ''}
             style={{ width: '100%', height: '100%' }}
             attributionControl={false}
           >
